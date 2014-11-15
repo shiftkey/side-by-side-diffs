52490d6512bd92e25b88d055f1adbcc344893872
Brendan Forster <brendan@github.com>
2014-11-15 10:17:52 +0800

    dump some sample data into place

--- src/SideBySideDiffs/MainWindow.xaml.cs
+++ src/SideBySideDiffs/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows;
 using System.Windows.Controls;
 using ICSharpCode.AvalonEdit;
 
@@ -14,21 +17,33 @@ namespace SideBySideDiffs
 
         void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-            // TODO: hack out the patch for a diff
+            // this is the format you can use to generate a "good enough" raw diff
+            // $ git difftool HEAD~1 -y -x "diff --old-line-format=\"%dn: - %L\" --new-line-format=\"%dn: + %L\" --unchanged-line-format=\"%dn:   %L\"" > output.txt
+
+            string diffContents = "";
+
+            var names = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+
+            var outputResourceKey = names.First(x => x.EndsWith("output.txt"));
+
+            using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(outputResourceKey))
+            using (var streamReader = new StreamReader(stream))
+            {
+                diffContents = streamReader.ReadToEnd();
+            }
+
+            diffContents = diffContents.Replace("\r\r\n", "\r\n");
+
+
+            left.Text = diffContents;
+            right.Text = diffContents;
+
             // TODO: split this into left and right pieces
             // TODO: bind this to the page
             // TODO: style this mofo
             // TODO: parse the strings into proper domain objects
             // TODO: introduce line highlighting
             // TODO: introduce highlighting specific sections
-
-            var left = new TextEditor();
-            Grid.SetColumn(left, 0);
-            rootGrid.Children.Add(left);
-
-            var right = new TextEditor();
-            Grid.SetColumn(right, 1);
-            rootGrid.Children.Add(right);
         }
     }
 }
